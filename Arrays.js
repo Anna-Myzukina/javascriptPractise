de989420fd4d12f9be6672a9ea95fe16c538657e
Anna <anna.muzykina83@gmail.com>
2019-06-29 17:00:09 +0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -7,3 +7,21 @@ Task
 Complete the getSecondLargest function in the editor below. It has one parameter: an array,nums , of n numbers. 
 The function must find and return the second largest number in nums.
 */
+function getSecondLargest(nums) {
+    let largest = nums[0];
+    let secondLargest = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+            continue;
+        }
+        
+        if ((nums[i] > secondLargest) && (nums[i] < largest)) {
+            secondLargest = nums[i];
+        }
+    }
+    
+    return secondLargest;
+}
