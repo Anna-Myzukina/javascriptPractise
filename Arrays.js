7325b3dc0760c825d7be87fb01f87eb7defea21d
Anna <anna.muzykina83@gmail.com>
2019-06-29 16:52:09 +0300

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+/*
+
+In this challenge, we learn about Arrays. Check out the attached tutorial for more details.
+
+Task
+
+Complete the getSecondLargest function in the editor below. It has one parameter: an array,nums , of n numbers. 
+The function must find and return the second largest number in nums.
+*/
